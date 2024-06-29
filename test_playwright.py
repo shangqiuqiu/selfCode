51bf0fe145189cf765b31ec11c4d77c3e57755e2
shangqiuqiu <shangqiuyoung@gmail.com>
2024-06-29 11:51:36 +0800

    Update test_playwright.py

--- test_playwright.py
+++ test_playwright.py
@@ -1,13 +1,11 @@
 from playwright.sync_api import sync_playwright
-# 上海悠悠 wx:283340479
-# blog:https://www.cnblogs.com/yoyoketang/
 
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=1000)
-    context = browser.new_context()  # 创建上下文，浏览器实例1
+    context = browser.new_context()  
 
-    page = context.new_page()    # 打开标签页
+    page = context.new_page() 
     page.goto("https://www.baidu.com/")
     print(page.title())
     page.screenshot(path="screenshot.png")
